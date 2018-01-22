d8494bfb203e982347b7af10cd740030138dd683
Caj Larsson <contact@caj.me>
2018-01-22 18:01:41 +0800

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='schemamacros',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/caj-larsson/schemamacros',
     license='MIT',
     author='Caj Larsson',
